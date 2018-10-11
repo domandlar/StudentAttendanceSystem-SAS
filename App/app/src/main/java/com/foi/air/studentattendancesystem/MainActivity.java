ce405ececb74798cc5fc047bb983e7907c9c8ada
Matija Benotić <mbenotic@gmail.com>
2018-10-11 22:07:40 +0200

    Kreiran prazan projekt

---
+++ App/app/src/main/java/com/foi/air/studentattendancesystem/MainActivity.java
@@ -0,0 +1,13 @@
+package com.foi.air.studentattendancesystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
