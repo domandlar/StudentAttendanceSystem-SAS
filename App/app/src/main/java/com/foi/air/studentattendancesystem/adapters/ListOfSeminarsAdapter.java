1469ed9a11a01cd204aefc3b16c1e0d9d499fe68
Matija Benotić <mbenotic@gmail.com>
2018-10-23 12:29:28 +0200

    Implementiran RecycleView i CardView za seminare s TESTNIM podacima!

--- App/app/src/main/java/com/foi/air/studentattendancesystem/adapters/ListOfSeminarsAdapter.java
+++ App/app/src/main/java/com/foi/air/studentattendancesystem/adapters/ListOfSeminarsAdapter.java
@@ -1,4 +1,64 @@
 package com.foi.air.studentattendancesystem.adapters;
 
-public class ListOfSeminarsAdapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.foi.air.studentattendancesystem.R;
+import com.foi.air.studentattendancesystem.ui.ListOfSeminars;
+
+import java.util.List;
+
+public class ListOfSeminarsAdapter extends RecyclerView.Adapter<ListOfSeminarsAdapter.SeminarViewHolder>{
+
+    private Context mCtx;
+    private List<Seminars> seminarsList;
+
+    public ListOfSeminarsAdapter(Context mCtx, List<Seminars> seminarsList) {
+        this.mCtx = mCtx;
+        this.seminarsList = seminarsList;
+    }
+
+    @NonNull
+    @Override
+    public SeminarViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        LayoutInflater inflater =LayoutInflater.from(mCtx);
+        View view = inflater.inflate(R.layout.adapter_list_of_seminars, null);
+        SeminarViewHolder holder = new SeminarViewHolder(view);
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SeminarViewHolder seminarViewHolder, int i) {
+        Seminars seminar = seminarsList.get(i);
+
+        seminarViewHolder.title.setText(seminar.getTitle());
+        seminarViewHolder.day.setText(seminar.getDay());
+        seminarViewHolder.time.setText(seminar.getTime());
+        seminarViewHolder.classroom.setText(seminar.getClassroom());
+    }
+
+    @Override
+    public int getItemCount() {
+        return seminarsList.size();
+    }
+
+    class SeminarViewHolder extends RecyclerView.ViewHolder {
+        TextView title, day, time, classroom;
+
+        public SeminarViewHolder(@NonNull View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.textViewTitle);
+            day = itemView.findViewById(R.id.textViewDay);
+            time = itemView.findViewById(R.id.textViewTime);
+            classroom = itemView.findViewById(R.id.textViewClassroom);
+
+
+        }
+    }
 }
--- App/app/src/main/java/com/foi/air/studentattendancesystem/ui/ListOfSeminars.java
+++ App/app/src/main/java/com/foi/air/studentattendancesystem/ui/ListOfSeminars.java
@@ -2,14 +2,59 @@ package com.foi.air.studentattendancesystem.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.foi.air.studentattendancesystem.R;
+import com.foi.air.studentattendancesystem.adapters.ListOfSeminarsAdapter;
+import com.foi.air.studentattendancesystem.adapters.Seminars;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListOfSeminars extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    ListOfSeminarsAdapter adapter;
+
+    List<Seminars> seminarsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_of_seminars);
+
+        seminarsList = new ArrayList<>();
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        //recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        seminarsList.add(
+                new Seminars(
+                        1,
+                        "Analiza i razvoj programa",
+                        "Petak",
+                        "14:00-16:00",
+                        "D9"));
+
+        seminarsList.add(
+                new Seminars(
+                        1,
+                        "Vanjskotrgovinsko poslovanje",
+                        "Srijeda",
+                        "17:00-18:00",
+                        "D10"));
+
+        seminarsList.add(
+                new Seminars(
+                        1,
+                        "Operacijski sustavi",
+                        "Utorak",
+                        "10:00-14:00",
+                        "D7"));
+
+        adapter=new ListOfSeminarsAdapter(this, seminarsList);
+        recyclerView.setAdapter(adapter);
     }
 }
