36bd2b6be451a17fee80a514e7d571b7219596d4
Matija Benotić <mbenotic@gmail.com>
2018-10-29 22:16:59 +0100

    Djelomično implementirano dodavanje seminara

--- App/app/src/main/java/com/foi/air/studentattendancesystem/ui_prof/ListOfSeminars.java
+++ App/app/src/main/java/com/foi/air/studentattendancesystem/ui_prof/ListOfSeminars.java
@@ -1,5 +1,8 @@
 package com.foi.air.studentattendancesystem.ui_prof;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -10,6 +13,7 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.foi.air.studentattendancesystem.R;
 import com.foi.air.studentattendancesystem.adapters_prof.ListOfSeminarsAdapter;
@@ -18,7 +22,7 @@ import com.foi.air.studentattendancesystem.adapters_prof.Seminars;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ListOfSeminars extends AppCompatActivity {
+public class ListOfSeminars extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private Toolbar toolBar;
 
@@ -38,6 +42,9 @@ public class ListOfSeminars extends AppCompatActivity {
         setSupportActionBar(toolBar);
 
         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolBar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
@@ -98,11 +105,31 @@ public class ListOfSeminars extends AppCompatActivity {
     }
 
     @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.nav_seminars:
+                Intent intent = new Intent(ListOfSeminars.this, ListOfSeminars.class);
+                startActivity(intent);
+        }
+        return true;
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.app_bar_menu,menu);
 
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_dodaj_seminar:
+                Intent intent = new Intent(ListOfSeminars.this, AddSeminar.class);
+                startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     //drawer
