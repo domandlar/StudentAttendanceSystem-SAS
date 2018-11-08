4f859f069ba4bfcbe4f75f32ec5736d100df2619
Tomislav Dubravac <tomi.i.dubravac@gmail.com>
2018-11-08 20:26:17 +0100

    Kreiran dizajn za login profesora

---
+++ App/app/src/main/java/com/foi/air/studentattendancesystem/uiprofesor/Login.java
@@ -0,0 +1,120 @@
+package com.foi.air.studentattendancesystem.uiprofesor;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.foi.air.studentattendancesystem.R;
+import com.foi.air.studentattendancesystem.uiprofesor.ListOfSeminars;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.w3c.dom.Text;
+
+public class Login extends AppCompatActivity implements View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
+
+    private Button buttonLogin;
+    private EditText editTextMail;
+    private EditText editTextPassword;
+    private TextView textViewRegistracija;
+
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if(firebaseAuth.getCurrentUser() != null){
+            finish();
+            startActivity(new Intent(getApplicationContext(), ListOfSeminars.class));
+        }
+
+
+        editTextMail = (EditText) findViewById(R.id.editTextEmail);
+
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+
+        buttonLogin = (Button) findViewById(R.id.buttonLogin);
+
+        textViewRegistracija = (TextView) findViewById(R.id.textViewRegistracija);
+
+        buttonLogin.setOnClickListener(this);
+
+        textViewRegistracija.setOnClickListener(this);
+
+        progressDialog = new ProgressDialog(this);
+    }
+
+    private void userLogin() {
+        String email = editTextMail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(this,"Unesite email adresu",Toast.LENGTH_SHORT).show();
+
+            return;
+        }
+
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Unesite lozinku", Toast.LENGTH_SHORT).show();
+
+            return;
+        }
+
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.dismiss();
+                        if(task.isSuccessful()){
+                            finish();
+                            startActivity(new Intent(getApplicationContext(), ListOfSeminars.class));
+                        }
+                    }
+                });
+
+
+    }
+
+
+
+
+
+    @Override
+    public void onClick(View view) {
+        if(view == buttonLogin) {
+            userLogin();
+        }
+
+        if(view == textViewRegistracija) {
+            //dovršiti kada bude gotova registracija
+        }
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.nav_logout:
+                Intent intent = new Intent(Login.this, Login.class);
+                startActivity(intent);
+        }
+        return true;
+    }
+}
