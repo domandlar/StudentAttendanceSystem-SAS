7672067a5fb6f90d7c6739278a30bf702963ea37
Matija Benotić <mbenotic@gmail.com>
2018-11-23 14:14:14 +0100

    Dodan okvirni SplashScreen u aplikaciju i okvirno dizajniran MainActivity

--- App/app/src/main/java/com/foi/air/studentattendancesystem/MainActivity.java
+++ App/app/src/main/java/com/foi/air/studentattendancesystem/MainActivity.java
@@ -1,15 +1,31 @@
 package com.foi.air.studentattendancesystem;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.foi.air.studentattendancesystem.R;
+import com.foi.air.studentattendancesystem.uiprofesor.ListOfSeminars;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mBtnStudent;
+    Button mBtnProfesor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBtnProfesor = findViewById(R.id.btnProfesor);
+        mBtnProfesor.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent (getApplicationContext(), Login.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
