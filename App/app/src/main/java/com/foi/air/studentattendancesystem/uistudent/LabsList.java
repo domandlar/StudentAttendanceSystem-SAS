8b9203796971c930c4158f04b041812180512f41
Domagoj <domagoj.andlar@gmail.com>
2019-01-09 20:23:39 +0100

    Implementiran ispis labosa za studenta.

--- App/app/src/main/java/com/foi/air/studentattendancesystem/uistudent/LabsList.java
+++ App/app/src/main/java/com/foi/air/studentattendancesystem/uistudent/LabsList.java
@@ -1,24 +1,157 @@
 package com.foi.air.studentattendancesystem.uistudent;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import com.foi.air.core.entities.Aktivnost;
+import com.foi.air.core.entities.Student;
+import com.foi.air.studentattendancesystem.MainActivity;
 import com.foi.air.studentattendancesystem.R;
+import com.foi.air.studentattendancesystem.adaptersprofesor.ListOfActivitiesAdapter;
+import com.foi.air.studentattendancesystem.loaders.SasWsDataLoadedListener;
+import com.foi.air.studentattendancesystem.loaders.SasWsDataLoader;
 
-public class LabsList extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LabsList extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, SasWsDataLoadedListener {
 
     private Toolbar toolBar;
 
     private DrawerLayout drawer;
 
+    RecyclerView recyclerView;
+    ListOfActivitiesAdapter adapter;
+
+    List<Aktivnost> labList;
+
+    Aktivnost aktivnost;
+
+    String idStudenta;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_labs_list);
+        setTitle("Moji Labosi");
 
+        toolBar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolBar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolBar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        //recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        idStudenta = prefs.getString("idStudenta", "");
+        Student student = new Student(Integer.parseInt(idStudenta));
+        aktivnost = new Aktivnost("Labosi");
+
+        //hohvacanje podataka sa servisa
+        SasWsDataLoader sasWsDataLoader = new SasWsDataLoader();
+        sasWsDataLoader.aktivnostForStudent(student, aktivnost, this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nav_seminars:
+                Intent intent = new Intent(LabsList.this, SeminarList.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_labs:
+                intent = new Intent(LabsList.this, LabsList.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_logout:
+                intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            case R.id.nav_schedule:
+                intent = new Intent(LabsList.this, ScheduleStudent.class);
+                startActivity(intent);
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.app_bar_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    //drawer
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public void onWsDataLoaded(Object message, String status, Object data) {
+        labList = new ArrayList<Aktivnost>();
+        String dataString = String.valueOf(data);
+        try {
+            JSONArray array = new JSONArray(dataString);
+            for (int i = 0; i < array.length(); i++) {
+                JSONObject row = array.getJSONObject(i);
+                Aktivnost novaAktivnost = new Aktivnost("Labos");
+                novaAktivnost.setIdAktivnosti(row.getInt("id"));
+                novaAktivnost.setKolegij(row.getString("kolegij"));
+                novaAktivnost.setDanIzvodenja(row.getString("dan_izvodenja"));
+                novaAktivnost.setPocetak(row.getString("pocetak"));
+                novaAktivnost.setKraj(row.getString("kraj"));
+                //novaAktivnost.setDozvoljenoIzostanaka(row.getInt("dozvoljeno_izostanaka"));
+                novaAktivnost.setDvorana(row.getString("dvorana"));
+                labList.add(novaAktivnost);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        adapter = new ListOfActivitiesAdapter(this, labList);
+        recyclerView.setAdapter(adapter);
     }
 }
