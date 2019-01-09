8c20aad35d4e8d631c1536759c767f1ac9924ccc
Domagoj <domagoj.andlar@gmail.com>
2019-01-09 20:39:14 +0100

    Kreiran activity za predbilježbu labosa.

---
+++ App/app/src/main/java/com/foi/air/studentattendancesystem/uistudent/LabsBooking.java
@@ -0,0 +1,15 @@
+package com.foi.air.studentattendancesystem.uistudent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.foi.air.studentattendancesystem.R;
+
+public class LabsBooking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_labs_booking);
+    }
+}
