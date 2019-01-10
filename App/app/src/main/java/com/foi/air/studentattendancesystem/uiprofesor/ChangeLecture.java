dd7df4d521f6f08ddf37d94ca7600b0435d0cdf3
domvitez <dominik.vitez@gmail.com>
2019-01-10 18:06:25 +0100

    Prikaz i dodavanje predavanja profesora

---
+++ App/app/src/main/java/com/foi/air/studentattendancesystem/uiprofesor/ChangeLecture.java
@@ -0,0 +1,215 @@
+package com.foi.air.studentattendancesystem.uiprofesor;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.foi.air.core.entities.Dvorana;
+import com.foi.air.core.entities.Kolegij;
+import com.foi.air.core.entities.Profesor;
+import com.foi.air.studentattendancesystem.R;
+import com.foi.air.studentattendancesystem.loaders.SasWsDataLoadedListener;
+import com.foi.air.studentattendancesystem.loaders.SasWsDataLoader;
+import com.weiwangcn.betterspinner.library.BetterSpinner;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class ChangeLecture extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, SasWsDataLoadedListener {
+
+    private Toolbar toolBar;
+    private DrawerLayout drawer;
+    private Button btnChangeLecture;
+    EditText mEditPocetakSata;
+    EditText mEditKrajSata;
+    EditText mEditDozvoljenoIzostanaka;
+    BetterSpinner spinnerKolegiji;
+    BetterSpinner spinnerDvorane;
+    BetterSpinner spinnerDani;
+
+    String idProfesora;
+
+    ArrayList<Kolegij> kolegijList;
+    ArrayList<Dvorana> dvoranaList;
+    ArrayAdapter<Kolegij> spinnerAdapterKolegiji;
+    ArrayAdapter<Dvorana> spinnerAdapterDvorane;
+    ArrayAdapter<CharSequence> spinnerAdapterDani;
+
+    int idKolegija=0;
+    int idDvorane=0;
+    String danOdrzavanja=null;
+    String pocetakSata=null;
+    String krajStata=null;
+    int dozvoljenoIzostanaka=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_lecture);
+
+
+
+        toolBar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolBar);
+
+        drawer = findViewById(R.id.drawer_layout);
+
+        //osluskivanje gumba "Moja predavanja"
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolBar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        idProfesora = prefs.getString("idProfesora", "");
+        Profesor profesor = new Profesor(Integer.parseInt(idProfesora));
+
+        SasWsDataLoader sasWsDataLoader = new SasWsDataLoader();
+        sasWsDataLoader.kolegijForProfesor(profesor,this);
+        sasWsDataLoader.Dvorane("predavaona");
+
+
+        pocetakSata=getIntent().getStringExtra("pocetak");
+        krajStata =getIntent().getStringExtra("kraj");
+        dozvoljenoIzostanaka=Integer.parseInt(getIntent().getStringExtra("dozvoljenoIzostanaka"));
+
+
+        spinnerKolegiji = findViewById(R.id.spinnerKolegiji);
+        spinnerAdapterKolegiji = new ArrayAdapter<Kolegij>(this, android.R.layout.simple_dropdown_item_1line, kolegijList);
+        spinnerKolegiji.setAdapter(spinnerAdapterKolegiji);
+        spinnerKolegiji.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long l) {
+                Kolegij kolegij = (Kolegij) parent.getItemAtPosition(position);
+                idKolegija = kolegij.getId();
+            }
+        });
+
+        spinnerDvorane = findViewById(R.id.spinnerDvorane);
+        spinnerAdapterDvorane = new ArrayAdapter<Dvorana>(this, android.R.layout.simple_dropdown_item_1line, dvoranaList);
+        spinnerDvorane.setAdapter(spinnerAdapterDvorane);
+        spinnerDvorane.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long l) {
+                Dvorana dvorana = (Dvorana) parent.getItemAtPosition(position);
+                idDvorane = dvorana.getIdDvorane();
+            }
+        });
+
+        spinnerDani = findViewById(R.id.spinnerDanOdrzavanja);
+        spinnerAdapterDani = ArrayAdapter.createFromResource(this,R.array.dani_u_tjednu,android.R.layout.simple_dropdown_item_1line);
+        spinnerDani.setAdapter(spinnerAdapterDani);
+        spinnerDani.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long l) {
+                danOdrzavanja = String.valueOf(parent.getItemAtPosition(position));
+            }
+        });
+
+        mEditPocetakSata=findViewById(R.id.editTextPocetak);
+        mEditKrajSata = findViewById(R.id.editTextKraj);
+        mEditDozvoljenoIzostanaka = findViewById(R.id.editTextDozvoljenoIzostanaka);
+
+        mEditPocetakSata.setText(getIntent().getStringExtra("pocetak"));
+        mEditKrajSata.setText(getIntent().getStringExtra("kraj"));
+        mEditDozvoljenoIzostanaka.setText(getIntent().getStringExtra("dozvoljenoIzostanaka"));
+
+
+        btnChangeLecture = findViewById(R.id.buttonAzurirajPredavanje);
+        btnChangeLecture.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if(idKolegija !=0 && idDvorane !=0 && danOdrzavanja != null && pocetakSata != null && krajStata != null && dozvoljenoIzostanaka !=0){
+                    mEditPocetakSata = findViewById(R.id.editTextPocetak);
+                    pocetakSata = mEditPocetakSata.getText().toString();
+                    mEditKrajSata = findViewById(R.id.editTextKraj);
+                    krajStata = mEditKrajSata.getText().toString();
+                    mEditDozvoljenoIzostanaka = findViewById(R.id.editTextDozvoljenoIzostanaka);
+                    dozvoljenoIzostanaka = Integer.parseInt(mEditDozvoljenoIzostanaka.getText().toString());
+                    SasWsDataLoader sasWsDataLoader = new SasWsDataLoader();
+                    sasWsDataLoader.dodajSeminar(Integer.parseInt(idProfesora),idKolegija,dozvoljenoIzostanaka,pocetakSata,krajStata,danOdrzavanja,idDvorane,"Predavanje");
+                    Toast.makeText(getApplicationContext(),"Predavanje je ažurirano!", Toast.LENGTH_SHORT).show();
+                }else{
+                    AlertDialog alertDialog = new AlertDialog.Builder(ChangeLecture.this).create();
+                    alertDialog.setTitle("Pogreška");
+                    alertDialog.setMessage("Niste unjeli sva polja!");
+                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+                    alertDialog.show();
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.nav_seminars:
+                Intent intent = new Intent(ChangeLecture.this, ListOfLectures.class);
+                startActivity(intent);
+        }
+        return true;
+    }
+
+    @Override
+    public void onWsDataLoaded(Object message, String status, Object data) {
+        if(status.equals("OK") && message.equals("Pronađeni kolegiji.")){
+            kolegijList = new ArrayList<Kolegij>();
+            String dataStringKolegij = String.valueOf(data);
+            try {
+                JSONArray array = new JSONArray(dataStringKolegij);
+                for (int i = 0; i < array.length(); i++) {
+                    JSONObject row = array.getJSONObject(i);
+                    Kolegij noviKolegij= new Kolegij(row.getInt("id"),row.getString("naziv"), row.getInt("semestar"), row.getString("studij"));
+                    kolegijList.add(noviKolegij);
+                }
+                spinnerAdapterKolegiji = new ArrayAdapter<Kolegij>(getApplicationContext(), android.R.layout.simple_dropdown_item_1line, kolegijList);
+                spinnerKolegiji.setAdapter(spinnerAdapterKolegiji);
+                spinnerAdapterKolegiji.notifyDataSetChanged();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }else if(status.equals("OK") && message.equals("Pronađene dvorane.")){
+            dvoranaList = new ArrayList<Dvorana>();
+            String dataStringDvorane = String.valueOf(data);
+            try {
+                JSONArray array = new JSONArray(dataStringDvorane);
+                for (int i = 0; i < array.length(); i++) {
+                    JSONObject row = array.getJSONObject(i);
+                    Dvorana novaDvorana= new Dvorana(row.getInt("id_dvorane"),row.getString("naziv"), row.getInt("kapacitet"));
+                    dvoranaList.add(novaDvorana);
+                }
+                spinnerAdapterDvorane = new ArrayAdapter<Dvorana>(getApplicationContext(), android.R.layout.simple_dropdown_item_1line, dvoranaList);
+                spinnerDvorane.setAdapter(spinnerAdapterDvorane);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
---
+++ App/app/src/main/java/com/foi/air/studentattendancesystem/uiprofesor/ListOfLectures.java
@@ -0,0 +1,164 @@
+package com.foi.air.studentattendancesystem.uiprofesor;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.foi.air.core.entities.Aktivnost;
+import com.foi.air.core.entities.Profesor;
+import com.foi.air.studentattendancesystem.MainActivity;
+import com.foi.air.studentattendancesystem.R;
+import com.foi.air.studentattendancesystem.adaptersprofesor.ListOfLecturesAdapter;
+import com.foi.air.studentattendancesystem.loaders.SasWsDataLoadedListener;
+import com.foi.air.studentattendancesystem.loaders.SasWsDataLoader;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListOfLectures extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, SasWsDataLoadedListener {
+
+    private Toolbar toolBar;
+
+    private DrawerLayout drawer;
+
+    RecyclerView recyclerView;
+    ListOfLecturesAdapter adapter;
+
+    List<Aktivnost> lectureList;
+
+    Aktivnost aktivnost;
+
+    String idProfesora;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_of_lectures);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        idProfesora = prefs.getString("idProfesora", "");
+
+        Profesor profesor = new Profesor(Integer.parseInt(idProfesora));
+        aktivnost = new Aktivnost("Predavanje");
+
+        toolBar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolBar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolBar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        //recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+        //hohvacanje podataka sa servisa
+        SasWsDataLoader sasWsDataLoader = new SasWsDataLoader();
+        sasWsDataLoader.aktivnostForProfesor(profesor,aktivnost,this);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.nav_seminars:
+                Intent intent = new Intent(ListOfLectures.this, ListOfLectures.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_courses:
+                intent = new Intent(ListOfLectures.this, ListOfCourses.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_labs:
+                intent = new Intent(ListOfLectures.this, ListOfLabs.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_lectures:
+                intent = new Intent(ListOfLectures.this, ListOfLectures.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_logout:
+                intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                finish();
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.app_bar_menu_lecture,menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_dodaj_predavanje:
+                Intent intent = new Intent(ListOfLectures.this, AddLecture.class);
+                startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    //drawer
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public void onWsDataLoaded(Object message, String status, Object data) {
+        lectureList = new ArrayList<Aktivnost>();
+        String dataString = String.valueOf(data);
+        try {
+            JSONArray array = new JSONArray(dataString);
+            for (int i = 0; i < array.length(); i++) {
+                JSONObject row = array.getJSONObject(i);
+                Aktivnost novaAktivnost = new Aktivnost("Predavanje");
+                novaAktivnost.setIdAktivnosti(row.getInt("id"));
+                novaAktivnost.setKolegij(row.getString("kolegij"));
+                novaAktivnost.setDanIzvodenja(row.getString("dan_izvodenja"));
+                novaAktivnost.setPocetak(row.getString("pocetak"));
+                novaAktivnost.setKraj(row.getString("kraj"));
+                //novaAktivnost.setDozvoljenoIzostanaka(row.getInt("dozvoljeno_izostanaka"));
+                novaAktivnost.setDvorana(row.getString("dvorana"));
+                lectureList.add(novaAktivnost);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        adapter=new ListOfLecturesAdapter(this, lectureList);
+        recyclerView.setAdapter(adapter);
+    }
+}
