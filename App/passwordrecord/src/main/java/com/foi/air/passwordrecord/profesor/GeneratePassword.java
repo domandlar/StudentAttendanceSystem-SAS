c261e9033320f9f10e01f82f5ecb3eee9950e047
Matija Benotić <mbenotic@gmail.com>
2019-01-11 13:25:01 +0100

    Dodan novi modul za generiranje lozinki, dodan fragment za generiranje lozinki (profesor) i dodan fragment za potvrdu prisustva (student)

---
+++ App/passwordrecord/src/main/java/com/foi/air/passwordrecord/profesor/GeneratePassword.java
@@ -0,0 +1,17 @@
+package com.foi.air.passwordrecord.profesor;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class GeneratePassword extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(com.foi.air.core.R.layout.fragment_generate_password, container, false);
+    }
+}
---
+++ App/passwordrecord/src/main/java/com/foi/air/passwordrecord/student/SubmitAttendance.java
@@ -0,0 +1,17 @@
+package com.foi.air.passwordrecord.student;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SubmitAttendance extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(com.foi.air.core.R.layout.fragment_submit_attendance, container, false);
+    }
+}
