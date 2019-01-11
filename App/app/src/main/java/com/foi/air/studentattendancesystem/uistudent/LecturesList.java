18cf788cb591d1213fc00dea173977348733cba1
domvitez <dominik.vitez@gmail.com>
2019-01-11 10:45:38 +0100

    Dopunjena navigacija za studenta

--- App/app/src/main/java/com/foi/air/studentattendancesystem/uistudent/LecturesList.java
+++ App/app/src/main/java/com/foi/air/studentattendancesystem/uistudent/LecturesList.java
@@ -2,6 +2,7 @@ package com.foi.air.studentattendancesystem.uistudent;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
@@ -9,57 +10,56 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.foi.air.core.entities.Aktivnost;
 import com.foi.air.core.entities.Profesor;
-import com.foi.air.core.entities.Seminar;
-import com.foi.air.core.entities.Student;
 import com.foi.air.studentattendancesystem.MainActivity;
 import com.foi.air.studentattendancesystem.R;
-import com.foi.air.studentattendancesystem.adaptersStudent.SeminarListAdapter;
-import com.foi.air.studentattendancesystem.adaptersprofesor.ListOfActivitiesAdapter;
+import com.foi.air.studentattendancesystem.adaptersprofesor.ListOfLecturesAdapter;
 import com.foi.air.studentattendancesystem.loaders.SasWsDataLoadedListener;
 import com.foi.air.studentattendancesystem.loaders.SasWsDataLoader;
-import com.foi.air.studentattendancesystem.uiprofesor.AddSeminar;
+import com.foi.air.studentattendancesystem.uiprofesor.AddLecture;
+import com.foi.air.studentattendancesystem.uiprofesor.ListOfCourses;
 import com.foi.air.studentattendancesystem.uiprofesor.ListOfLabs;
-import com.foi.air.studentattendancesystem.uiprofesor.ListOfLectures;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
-public class SeminarList extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, SasWsDataLoadedListener {
+public class LecturesList extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, SasWsDataLoadedListener {
 
     private Toolbar toolBar;
 
     private DrawerLayout drawer;
 
     RecyclerView recyclerView;
-    ListOfActivitiesAdapter adapter;
+    ListOfLecturesAdapter adapter;
 
-    List<Aktivnost> seminarList;
+    List<Aktivnost> lectureList;
 
     Aktivnost aktivnost;
 
-    String idStudenta;
+    String idProfesora;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_seminar_list);
-        setTitle("Moji Seminari");
+        setContentView(R.layout.activity_list_of_lectures);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        idProfesora = prefs.getString("idProfesora", "");
+
+        Profesor profesor = new Profesor(Integer.parseInt(idProfesora));
+        aktivnost = new Aktivnost("Predavanje");
 
         toolBar = findViewById(R.id.toolBar);
         setSupportActionBar(toolBar);
@@ -68,7 +68,7 @@ public class SeminarList extends AppCompatActivity implements NavigationView.OnN
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolBar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolBar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
@@ -78,44 +78,35 @@ public class SeminarList extends AppCompatActivity implements NavigationView.OnN
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
 
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        idStudenta = prefs.getString("idStudenta", "");
-        Student student = new Student(Integer.parseInt(idStudenta));
-        aktivnost = new Aktivnost("Seminar");
-
         //hohvacanje podataka sa servisa
         SasWsDataLoader sasWsDataLoader = new SasWsDataLoader();
-        sasWsDataLoader.aktivnostForStudent(student, aktivnost, this);
+        sasWsDataLoader.aktivnostForProfesor(profesor,aktivnost,this);
 
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        switch (menuItem.getItemId()) {
+        switch (menuItem.getItemId()){
             case R.id.nav_seminars:
-                Intent intent = new Intent(SeminarList.this, SeminarList.class);
+                Intent intent = new Intent(LecturesList.this, LecturesList.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_courses:
+                intent = new Intent(LecturesList.this, ListOfCourses.class);
                 startActivity(intent);
                 break;
             case R.id.nav_labs:
-                intent = new Intent(SeminarList.this, LabsList.class);
+                intent = new Intent(LecturesList.this, ListOfLabs.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_lectures:
+                intent = new Intent(LecturesList.this, LecturesList.class);
                 startActivity(intent);
                 break;
             case R.id.nav_logout:
                 intent = new Intent(this, MainActivity.class);
                 startActivity(intent);
                 finish();
-            case R.id.nav_schedule:
-                intent = new Intent(SeminarList.this, ScheduleStudent.class);
-                startActivity(intent);
-                break;
-            case R.id.nav_courses:
-                intent = new Intent(SeminarList.this, ListCourses.class);
-                startActivity(intent);
-                break;
-            case R.id.nav_lectures:
-                intent = new Intent(SeminarList.this, ListOfLectures.class);
-                startActivity(intent);
-                break;
         }
         return true;
     }
@@ -123,15 +114,17 @@ public class SeminarList extends AppCompatActivity implements NavigationView.OnN
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.app_bar_menu, menu);
+        menuInflater.inflate(R.menu.app_bar_menu_lecture,menu);
 
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-
+        switch (item.getItemId()){
+            case R.id.action_dodaj_predavanje:
+                Intent intent = new Intent(LecturesList.this, AddLecture.class);
+                startActivity(intent);
         }
         return super.onOptionsItemSelected(item);
     }
@@ -139,22 +132,22 @@ public class SeminarList extends AppCompatActivity implements NavigationView.OnN
     //drawer
     @Override
     public void onBackPressed() {
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
+        if(drawer.isDrawerOpen(GravityCompat.START)){
             drawer.closeDrawer(GravityCompat.START);
-        } else {
+        }else {
             super.onBackPressed();
         }
     }
 
     @Override
     public void onWsDataLoaded(Object message, String status, Object data) {
-        seminarList = new ArrayList<Aktivnost>();
+        lectureList = new ArrayList<Aktivnost>();
         String dataString = String.valueOf(data);
         try {
             JSONArray array = new JSONArray(dataString);
             for (int i = 0; i < array.length(); i++) {
                 JSONObject row = array.getJSONObject(i);
-                Aktivnost novaAktivnost = new Aktivnost("Seminar");
+                Aktivnost novaAktivnost = new Aktivnost("Predavanje");
                 novaAktivnost.setIdAktivnosti(row.getInt("id"));
                 novaAktivnost.setKolegij(row.getString("kolegij"));
                 novaAktivnost.setDanIzvodenja(row.getString("dan_izvodenja"));
@@ -162,14 +155,13 @@ public class SeminarList extends AppCompatActivity implements NavigationView.OnN
                 novaAktivnost.setKraj(row.getString("kraj"));
                 //novaAktivnost.setDozvoljenoIzostanaka(row.getInt("dozvoljeno_izostanaka"));
                 novaAktivnost.setDvorana(row.getString("dvorana"));
-                seminarList.add(novaAktivnost);
+                lectureList.add(novaAktivnost);
             }
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
-        adapter = new ListOfActivitiesAdapter(this, seminarList);
+        adapter=new ListOfLecturesAdapter(this, lectureList);
         recyclerView.setAdapter(adapter);
     }
 }
-
