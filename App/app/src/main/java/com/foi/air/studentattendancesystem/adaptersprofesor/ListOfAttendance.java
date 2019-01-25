7ff739f207fbdf5174aa5b3d496b20a2eb702219
ACA_BRUTALNI\Lukas <lukkristi@foi.hr>
2019-01-25 10:46:29 +0100

    Kreirana klasa ListAttendance i adapter za prikaz provjere dolazaka i nedolazaka

---
+++ App/app/src/main/java/com/foi/air/studentattendancesystem/adaptersprofesor/ListOfAttendance.java
@@ -0,0 +1,15 @@
+package com.foi.air.studentattendancesystem.adaptersprofesor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.foi.air.studentattendancesystem.R;
+
+public class ListOfAttendance extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.adapter_list_of_attendance);
+    }
+}
