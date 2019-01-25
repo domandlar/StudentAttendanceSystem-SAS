87a0622ace05a1729de8d170bbb0ea8dc0a4f00f
ACA_BRUTALNI\Lukas <lukkristi@foi.hr>
2019-01-25 12:13:13 +0100

    Kreirana i implementirana klasa u core modulu

---
+++ App/core/src/main/java/com/foi/air/core/entities/Dolazak.java
@@ -0,0 +1,26 @@
+package com.foi.air.core.entities;
+
+public class Dolazak {
+    int tjedanNastave;
+    boolean prisustvo;
+
+    public Dolazak(){
+        
+    }
+
+    public int getTjedanNastave() {
+        return tjedanNastave;
+    }
+
+    public void setTjedanNastave(int tjedanNastave) {
+        this.tjedanNastave = tjedanNastave;
+    }
+
+    public void setPrisustvo(boolean prisustvo) {
+        this.prisustvo = prisustvo;
+    }
+
+    public boolean isPrisustvo() {
+        return prisustvo;
+    }
+}
