7d7503f43e75e393e3e3e381eabe3290e2ac90c5
Domagoj <domagoj.andlar@gmail.com>
2019-01-29 18:52:22 +0100

    Implementirano detektiranje lica i prepoznavanje lica na slici.

--- App/facerecognition/src/main/java/com/example/facerecognition/uiprofesor/CheckingAttendance.java
+++ App/facerecognition/src/main/java/com/example/facerecognition/uiprofesor/CheckingAttendance.java
@@ -1,10 +1,7 @@
 package com.example.facerecognition.uiprofesor;
 
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
@@ -15,20 +12,42 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 import com.example.facerecognition.R;
-import com.foi.air.core.NavigationItem;
-import com.foi.air.core.entities.Student;
-
-import java.util.List;
+import com.microsoft.projectoxford.face.FaceServiceClient;
+import com.microsoft.projectoxford.face.FaceServiceRestClient;
+import com.microsoft.projectoxford.face.contract.FaceRectangle;
 
 import static android.app.Activity.RESULT_OK;
+
+import com.microsoft.projectoxford.face.contract.*;
+import com.microsoft.projectoxford.face.contract.Person;
+import com.microsoft.projectoxford.face.rest.ClientException;
+
+import java.io.*;
+import java.util.UUID;
+
+import android.app.*;
+import android.os.*;
+import android.graphics.*;
+import android.widget.*;
 
 
 public class CheckingAttendance extends Fragment {
     static final int REQUEST_IMAGE_CAPTURE = 1;
-    ImageView slika;
+    ImageView imageView;
     Button btnSlikaj;
     Button btnProvijeri;
     View rootView;
+
+    Bitmap mBitmap;
+    Face[] facesDetected;
+
+    private final String apiEndpoint = "https://westeurope.api.cognitive.microsoft.com/face/v1.0";
+
+    private final String subscriptionKey = "ee85c084b99f4ebc9c8c5c9101105f4d";
+    private final String personGroupId = "students";
+
+    private final FaceServiceClient faceServiceClient =
+            new FaceServiceRestClient(apiEndpoint, subscriptionKey);
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -36,7 +55,7 @@ public class CheckingAttendance extends Fragment {
         // Inflate the layout for this fragment
         rootView = inflater.inflate(R.layout.fragment_checking_attendance, container, false);
 
-        slika = rootView.findViewById(R.id.cameraView);
+        imageView = rootView.findViewById(R.id.cameraView);
         btnSlikaj = rootView.findViewById(R.id.btnPoslikaj);
         btnProvijeri = rootView.findViewById(R.id.btnProvijeri);
 
@@ -47,22 +66,179 @@ public class CheckingAttendance extends Fragment {
                 startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
             }
         });
+        btnProvijeri.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                mBitmap.compress(Bitmap.CompressFormat.JPEG,100,outputStream);
+                ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+
+                new detectTask().execute(inputStream);
+            }
+        });
 
         return rootView;
     }
 
 
 
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-    }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
-            slika.setImageBitmap(imageBitmap);
+            mBitmap = imageBitmap;
+            imageView.setImageBitmap(imageBitmap);
+
         }
     }
+
+    class detectTask extends  AsyncTask<InputStream,String,Face[]> {
+        private ProgressDialog mDialog = new ProgressDialog(getContext());
+
+
+        @Override
+        protected Face[] doInBackground(InputStream... params) {
+            try{
+                publishProgress("Traženje...");
+                Face[] results = faceServiceClient.detect(params[0],true,false,null);
+                if(results == null)
+                {
+                    publishProgress("Traženje je završeno. Nisu pronađena lica na slici.");
+                    return null;
+                }
+                else{
+                    publishProgress(String.format("Traženje je završeno. %d studenta(s) pronađeno",results.length));
+                    return results;
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPreExecute() {
+            mDialog.show();
+        }
+
+        @Override
+        protected void onPostExecute(Face[] faces) {
+            mDialog.dismiss();
+
+            facesDetected = faces;
+
+            //Prepoznavanje
+            final UUID[] faceIds = new UUID[facesDetected.length];
+            for(int i=0;i<facesDetected.length;i++){
+                faceIds[i] = facesDetected[i].faceId;
+            }
+
+            new IdentificationTask(personGroupId).execute(faceIds);
+
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            mDialog.setMessage(values[0]);
+        }
+
+    }
+
+    private class IdentificationTask extends AsyncTask<UUID,String,IdentifyResult[]> {
+        String personGroupId;
+
+        private ProgressDialog mDialog = new ProgressDialog(getContext());
+
+        public IdentificationTask(String personGroupId) {
+            this.personGroupId = personGroupId;
+        }
+
+        @Override
+        protected IdentifyResult[] doInBackground(UUID... params) {
+
+            try{
+                publishProgress("Dohvaćanje statusa grupe...");
+                TrainingStatus trainingStatus  = faceServiceClient.getPersonGroupTrainingStatus(this.personGroupId);
+                if(trainingStatus.status != TrainingStatus.Status.Succeeded)
+                {
+                    publishProgress("Trening status grupe je "+trainingStatus.status);
+                    return null;
+                }
+                publishProgress("Identifikacija...");
+
+                IdentifyResult[] results = faceServiceClient.identity(personGroupId, // person group id
+                        params // face ids
+                        ,1); // max number of candidates returned
+
+                return results;
+
+            } catch (Exception e)
+            {
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPreExecute() {
+            mDialog.show();
+        }
+
+        @Override
+        protected void onPostExecute(IdentifyResult[] identifyResults) {
+            mDialog.dismiss();
+
+            for(IdentifyResult identifyResult:identifyResults)
+            {
+                if(identifyResult.candidates.size() > 0)
+                    new PersonDetectionTask(personGroupId).execute(identifyResult.candidates.get(0).personId);
+            }
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            mDialog.setMessage(values[0]);
+        }
+    }
+
+    private class PersonDetectionTask extends AsyncTask<UUID,String,Person> {
+        private ProgressDialog mDialog = new ProgressDialog(getContext());
+        private String personGroupId;
+
+        public PersonDetectionTask(String personGroupId) {
+            this.personGroupId = personGroupId;
+        }
+
+        @Override
+        protected Person doInBackground(UUID... params) {
+            try{
+                publishProgress("Dohvaćanje osobe...");
+
+                Person person = faceServiceClient.getPerson(personGroupId,params[0]);
+                return person;
+            } catch (Exception e)
+            {
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPreExecute() {
+            mDialog.show();
+        }
+
+        @Override
+        protected void onPostExecute(Person person) {
+            mDialog.dismiss();
+
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            mDialog.setMessage(values[0]);
+        }
+    }
+
 }
